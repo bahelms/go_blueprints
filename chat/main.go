9d624358965a76460d699a87eff18fb5361174f2
Barrett Helms <barrett.helms@salesloft.com>
2019-01-15 21:41:08 -0500

    Hooks up websockets between server/client

--- chat/main.go
+++ chat/main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>Chat</title>
-        </head>
-        <body>
-          Let's chat!
-        </body>
-      </html>
-    `))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		fp := filepath.Join("templates", t.filename)
+		t.templ = template.Must(template.ParseFiles(fp))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run() // start room
 
 	// start server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
